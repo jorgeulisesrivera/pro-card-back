81f728cccfb42eca6a3c37137cf4ed3ecc606ce7
Jorge Rivera <jorgeulisesrivera.a@gmail.com>
2020-08-30 18:28:19 -0400

    connected to db!

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
+require('dotenv').config()
 const { buildSchema } = require('graphql')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
-const  cors = require('cors');
+const cors = require('cors')
 
 const app = express()
 const port = process.env.port || 4000
@@ -14,19 +15,8 @@ const port = process.env.port || 4000
 const schema = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8'))
 
 app.use(
-    cors({ 
-        origin: '*', 
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: [
-            'Content-Type', 
-            'Authorization', 
-            'Origin', 
-            'x-access-token', 
-            'XSRF-TOKEN'
-        ], 
-        preflightContinue: false 
-    })
-);
+  cors({ origin: '*' })
+)
 
 app.use('/api', graphqlHTTP({
   schema: schema,
